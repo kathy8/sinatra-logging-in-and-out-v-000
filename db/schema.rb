8691939df4c3a0ca81722cff3d067e23e1c6e659
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-30 18:33:03 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,5 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
+  create-table "users," force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.float "balance"
+  end
 
 end
