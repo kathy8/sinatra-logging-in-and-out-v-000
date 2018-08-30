20a3ec90d38b6e270264b54fe1bd9c6eec4bb1b0
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-30 18:37:38 +0000

    automatically backed up by learn

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -7,13 +7,13 @@ describe "Helpers" do
       session = {
         :user_id => 1
       }
-      
+
       expect(Helpers.current_user(session)).to be_an_instance_of(User)
     end
 
   end
 
-  describe 'Helpers.is_logged_in?' do 
+  describe 'Helpers.is_logged_in?' do
     it "returns true or false" do
       @user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
 
